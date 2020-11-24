e4b854b3cf0c299599615ede630985223af6e4ef
Fulano2141 <macuto2142dt@gmail.com>
2020-11-24 19:50:33 -0400

    static charts

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-      <router-link to="/crops/settings">Settings</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/crops/settings">Settings</router-link> |
       <router-link to="/crops/dashboard">Dashboard</router-link>
     </div>
     <router-view />
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,10 +1,52 @@
 <template>
-  <div></div>
+  <div>
+    <!-- <line-chart v-if="loaded" :chartdata="chartdata" :options="options" /> -->
+    <div class="container">
+      <div class="row">
+        <div class="col"><PieChart /></div>
+        <div class="col"><AreaChart /></div>
+      </div>
+    </div>
+    <hr>
+    <div class="container">
+      <div class="row">
+        <div class="col"><BarChart /></div>
+        <div class="col"><RadarChart /></div>
+      </div>
+    </div>
+    <hr>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm"></div>
+        <div class="col-sm"><LineChart /></div>
+        <div class="col-sm"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
-</script>
+import AreaChart from "../components/AreaChart.vue";
+import PieChart from "../components/PieChart.vue";
+import BarChart from "../components/BarChart.vue";
+import RadarChart from "../components/RadarChart.vue";
+import LineChart from "../components/LineChart.vue";
 
-<style>
-</style>
+export default {
+  name: "Dashboard",
+  components: {
+    AreaChart,
+    PieChart,
+    BarChart,
+    RadarChart,
+    LineChart,
+  },
+  data: () => ({
+    chartData: {
+      Books: 24,
+      Magazine: 30,
+      Newspapers: 10,
+    },
+  }),
+};
+</script>
