70bc952c38bceb0075666a693a90166de47be219
Fulano2141 <macuto2142dt@gmail.com>
2020-11-26 21:34:44 -0400

    organization

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,11 @@
         :src="item.src"
         reverse-transition="fade-transition"
         transition="fade-transition"
-      ></v-carousel-item>
+      >
+        <v-row class="fill-height" align="center" justify="center">
+          <div class="display-3">{{ item.text }}</div>
+        </v-row>
+      </v-carousel-item>
     </v-carousel>
     <CircularGauge :value="30" :height="100" />
   </div>
@@ -23,16 +27,24 @@ export default {
     model: 0,
     items: [
       {
-        src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+        src:
+          "https://everyone.plos.org/wp-content/uploads/sites/5/legacy-featured-images/fc-rice-16x9.jpg",
+        text: "Control de Aire",
       },
       {
-        src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+        src:
+          "https://www.isaaa.org/kc/cropbiotechupdate/files/images/2019-07-31-cornfield.jpg",
+        text: "Cultivos frescos",
       },
       {
-        src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+        src:
+          "https://www.isaaa.org/kc/cropbiotechupdate/files/images/182020120350AM.jpg",
+        text: "Productos saludables",
       },
       {
-        src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+        src:
+          "https://www.esa.int/var/esa/storage/images/esa_multimedia/images/2020/08/satellites_provide_data_on_crops/22165901-1-eng-GB/Satellites_provide_data_on_crops_pillars.jpg",
+        text: "No pierdas tu cosecha ;D",
       },
     ],
     colors: ["primary", "secondary", "yellow darken-2", "red", "orange"],
